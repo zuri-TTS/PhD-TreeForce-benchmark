b4ad998b6947dace934819abac853ec8e1c809b9
zuri <webzuri@gmail.com>
2022-02-02 10:34:04 +0100

    benchmark, update

---
+++ benchmark/rules/models/genetic_1000(1)_10000(1).php
@@ -0,0 +1,20 @@
+<?php return [array (
+  'site' => 1,
+  'people' => 1,
+  'person' => 1,
+  'address' => 1,
+  'closed_auctions' => 1,
+  'closed_auction' => 1,
+  'annotation' => 1,
+  'description' => 1,
+  'parlist' => 1,
+  'listitem' => 1,
+  'text' => 1,
+  'emph' => 1,
+  'bold' => 1,
+  '#nb' => 
+  array (
+    1000 => 1,
+    10000 => 1,
+  ),
+)];
---
+++ benchmark/rules/models/genetic_1000(400-425)_10000(400-425).php
@@ -0,0 +1,39 @@
+<?php return [array (
+  'site' => 5,
+  'people' => 4,
+  'person' => 4,
+  'address' => 5,
+  'closed_auctions' => 3,
+  'closed_auction' => 3,
+  'annotation' => 3,
+  'description' => 1,
+  'parlist' => 1,
+  'listitem' => 1,
+  'text' => 1,
+  'emph' => 1,
+  'bold' => 3,
+  '#nb' => 
+  array (
+    1000 => 400,
+    10000 => 405,
+  ),
+),array (
+  'site' => 5,
+  'people' => 5,
+  'person' => 4,
+  'address' => 4,
+  'closed_auctions' => 5,
+  'closed_auction' => 2,
+  'annotation' => 1,
+  'description' => 1,
+  'parlist' => 2,
+  'listitem' => 1,
+  'text' => 4,
+  'emph' => 1,
+  'bold' => 1,
+  '#nb' => 
+  array (
+    1000 => 400,
+    10000 => 400,
+  ),
+)];
