001e11f149bd12eb632ad4a9115addac1d9e23f6
zuri <webzuri@gmail.com>
2022-02-03 01:10:08 +0100

    autoload, handle namespaces

--- software/bench_scripts/classes/autoload.php
+++ software/bench_scripts/classes/autoload.php
@@ -1,5 +1,7 @@
 <?php
 \spl_autoload_register(function (string $className) {
+    $className = \str_replace('\\', '/', $className);
+
     $path = __DIR__ . "/$className.php";
 
     if (\is_file($path))
