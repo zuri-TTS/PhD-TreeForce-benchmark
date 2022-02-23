f20bce186fdf9e9a065747fc5f94d4f5bbe4c8e2
zuri <webzuri@gmail.com>
2022-02-23 15:24:01 +0100

    chlink, handles empty 'target' argument (ie. display all targets)

--- software/bench_scripts/chlink.php
+++ software/bench_scripts/chlink.php
@@ -13,9 +13,6 @@ $args = new class() extends stdClass {
 };
 $cmdArgsDef = [];
 
-if (empty($argv))
-    $argv[] = ";";
-
 $cmdParsed = \parseArgvShift($argv);
 $oargs = (new \Args\ObjectArgs($args));
 $oargs->updateAndShift($cmdParsed);
@@ -23,31 +20,38 @@ $groups = \array_filter_shift($cmdParsed, 'is_int', ARRAY_FILTER_USE_KEY);
 
 $oargs->checkEmpty($cmdParsed);
 
-if (empty($args->target))
-    throw new \Exception("Argument 'target' must be set");
-
-if (\count($groups) == 0) {
-    $groups = [
-        null
-    ];
-}
 $groups = \array_unique(DataSets::allGroups($groups));
 DataSets::checkGroupsNotExists($groups, false);
 
-foreach ($groups as $group) {
-    $link = "benchmark/{$args->mode}_conf/symlinks/$group";
-    $target = \str_replace('{}', $group, $args->target);
+if (empty($args->target)) {
 
-    echo "$link --> $target\n";
+    foreach ($groups as $group) {
+        $link = "benchmark/{$args->mode}_conf/symlinks/$group";
 
-    if (! \is_dir($target)) {
-        fwrite(STDERR, "WARNING! $target is not a directory\n");
-        continue;
+        if (\is_link($link))
+            $target = \readlink($link);
+        else
+            $target = '??';
+
+        echo "$link --> $target\n";
     }
-    $target = \realpath($target);
+} else {
 
-    if (\is_link($link))
-        \unlink($link);
+    foreach ($groups as $group) {
+        $link = "benchmark/{$args->mode}_conf/symlinks/$group";
+        $target = \str_replace('{}', $group, $args->target);
 
-    \symlink($target, $link);
+        echo "$link --> $target\n";
+
+        if (! \is_dir($target)) {
+            fwrite(STDERR, "WARNING! $target is not a directory\n");
+            continue;
+        }
+        $target = \realpath($target);
+
+        if (\is_link($link))
+            \unlink($link);
+
+        \symlink($target, $link);
+    }
 }
