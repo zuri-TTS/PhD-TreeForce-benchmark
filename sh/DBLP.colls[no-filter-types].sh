1df9628798571a6cb17f898efd30a135fe04a8ac
zuri <webzuri@gmail.com>
2022-04-02 01:13:28 +0200

    sh, updates

--- sh/DBLP.colls[no-filter-types].sh
+++ sh/DBLP.colls[no-filter-types].sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 scriptName=$(basename -s '.sh' "$0")
+group=DBLP.colls
 
 export SUMMARIES="key-type path"
 export PARAMS="Psummary.filter.types=n"
 
-com=$(sh/benchmark.sh 'DBLP[simplified,prefix-colls]' "outputs/$scriptName" $*)
+com=$(sh/benchmark.sh "$group[simplified]" "outputs/$scriptName" $*)
 eval "php $com"
