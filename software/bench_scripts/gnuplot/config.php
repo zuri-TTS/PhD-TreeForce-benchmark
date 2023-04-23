e8c17155c918796635a2de083d77420d3aabec2e
zuri <webzuri@gmail.com>
2023-04-24 00:37:32 +0200

    Plotter/, clean unused plotters

--- software/bench_scripts/gnuplot/config.php
+++ software/bench_scripts/gnuplot/config.php
@@ -1,13 +1,11 @@
 <?php
 return [
     'plotter.factory' => [
-        'each' => fn (Plot $plot) => new Plotter\EachPlotter($plot),
-        'group' => fn (Plot $plot) => new Plotter\GroupPlotter($plot),
         'full' => fn (Plot $plot) => new Plotter\FullPlotter($plot, new Plotter\FullStrategy()),
         'full-line' => fn (Plot $plot) => (new Plotter\FullPlotter($plot, new Plotter\FullLineStrategy()))->setTemplate('full-line.plot.php'),
         'full-aggregation' => fn (Plot $plot) => new Plotter\FullPlotter($plot, new Plotter\FullAggregationStrategy()),
-        'full-part' => fn (Plot $plot) => new Plotter\FullPartitioningPlotter(),
-        'full-ppart' => fn (Plot $plot) => new Plotter\FullParallelPartitioningPlotter(),
+        'full-part' => fn (Plot $plot) => new Plotter\FullPartitioningPlotter($plot),
+        'full-ppart' => fn (Plot $plot) => new Plotter\FullParallelPartitioningPlotter($plot),
         'noempty' => fn (Plot $plot) => new Plotter\FullNoEmptyPlotter($plot)
     ]
 ];
